7110285fd8c5868b5bb6e024861bf600ec160c8f
Will Young <will.young_@outlook.com>
2024-11-10 02:54:47 -0600

    Update db_test.go
    add godotenv load

--- db/db_test.go
+++ db/db_test.go
@@ -11,7 +11,9 @@ import (
 )
 
 func TestGetPageViews(t *testing.T) {
-
+	if err := godotenv.Load(); err != nil {
+		slog.Error("Error loading .env file", "error", err)
+	}
 	API_URL := os.Getenv("API_URL")
 	API_KEY := os.Getenv("API_KEY")
 
